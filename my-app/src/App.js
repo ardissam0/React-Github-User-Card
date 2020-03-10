918928fa7495cee47fa2d47e2a3b6db4508e193d
Sam Ardis <ardis.productions@gmail.com>
2020-03-10 12:52:21 -0400

    Added all dependencies and built out structure of files

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Github User Card</h1>
+      </header>
+
+      <GithubUser />
+    </div>
+  );
+}
+
+export default App;
